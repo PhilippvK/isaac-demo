1c73e8d614c5632a661e92c44bd4bf0a7101ad88
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-09-09 18:20:34 +0200

    add mlonmcu files

---
+++ scripts/setup_mlonmcu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TOP_DIR=$(dirname $SCRIPT_DIR)
+INSTALL_DIR=$TOP_DIR/install
+MLONMCU_TEMPLATE=$TOP_DIR/environment.yml.j2
+MLONMCU_HOME=$INSTALL_DIR/mlonmcu
+ETISS_DIR=$TOP_DIR/etiss
+ETISS_INSTALL_DIR=$INSTALL_DIR/etiss
+LLVM_INSTALL_DIR=$INSTALL_DIR/llvm
+
+TEMPLATE_LLVM_ARGS="-c llvm_install_dir=$LLVM_INSTALL_DIR"
+TEMPLATE_ETISS_ARGS="-c etiss_dir=$ETISS_DIR etiss_install_dir=$ETISS_INSTALL_DIR"
+
+python -m mlonmcu.cli.main init -t "$MLONMCU_TEMPLATE" "$MLONMCU_HOME" --non-interactive --allow-exists $TEMPLATE_LLVM_ARGS $TEMPLATE_ETISS_ARGS
+
+python -m mlonmcu.cli.main setup -g
+
+python -m pip install -r "$MLONMCU_HOME/requirements_addition.txt"
+
+python -m mlonmcu.cli.main setup -v --rebuild
