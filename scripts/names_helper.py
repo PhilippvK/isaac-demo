b569c4e7428aa7fb2d7be43ab4e1c243b9b9fa72
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-04-11 03:49:13 +0200

    scripts/names_helper.py: update

--- scripts/names_helper.py
+++ scripts/names_helper.py
@@ -1,24 +1,7 @@
 import argparse
-from typing import Union, List, Optional
-from pathlib import Path
 
 import yaml
 import pandas as pd
-
-
-def get_combined_score_df(
-    names_df: pd.DataFrame,
-    seal5_score_df: Optional[pd.DataFrame] = None,
-    mlonmcu_score_df: Optional[pd.DataFrame] = None,
-    static_counts_score_df: Optional[pd.DataFrame] = None,
-    dyn_counts_score_df: Optional[pd.DataFrame] = None,
-    static_enc_score_df: Optional[pd.DataFrame] = None,
-):
-    assert names_df is not None
-    combined_score_df = names_df[["instr", "instr_lower"]]
-    if seal5_score_df is not None:
-        combined_score_df.merge(seal5_score_df)
-    return combined_score_df
 
 
 def main():
@@ -30,10 +13,14 @@ def main():
     with open(args.index, "r") as f:
         combined_index_data = yaml.safe_load(f)
         # TODO: index and cdsl should use the same instruction names?
-        names = [f"CUSTOM{i}" for i, candidate in enumerate(combined_index_data["candidates"])]
+        names = [
+            candidate["properties"].get("InstrName", f"CUSTOM{i}")
+            for i, candidate in enumerate(combined_index_data["candidates"])
+        ]
         # num_candidates = len(names)
         names_df = pd.DataFrame({"instr": names})
         names_df["instr_lower"] = names_df["instr"].apply(lambda x: x.lower())
+        names_df["idx"] = names_df["instr"].apply(lambda x: names.index(x))
 
     if args.output is None:
         print(names_df)
