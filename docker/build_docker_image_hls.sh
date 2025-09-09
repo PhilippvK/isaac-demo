1fcf0cfe2c33d99b31b5098f088cff7aca97d5bb
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-09-09 09:09:37 +0200

    rename hls image

--- docker/build_docker_image_hls.sh
+++ docker/build_docker_image_hls.sh
@@ -4,7 +4,7 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TOP_DIR=$(dirname $SCRIPT_DIR)
-IMAGE=philippvk/hls-quickstart
+IMAGE=philippvk/isaac-quickstart-hls
 TAG=latest
 
 HLS_DIR=${HLS_DIR:-/path/to/isax-tools-integration}
