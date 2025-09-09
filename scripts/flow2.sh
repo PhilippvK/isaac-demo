207f8af5fd90fb59b867cf231ba439ca5cb5900a
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-09-09 07:21:59 +0200

    scripts/flow2.sh: support more targets and draft new mlonmcu frontend cli

--- scripts/flow2.sh
+++ scripts/flow2.sh
@@ -13,6 +13,7 @@ RUN=$DIR/run
 SESS=$DIR/sess
 WORK=$DIR/work
 
+TARGET=${TARGET:-etiss}
 FORCE_ARGS=""
 
 FORCE=1
@@ -25,6 +26,8 @@ python3 -m isaac_toolkit.session.create --session $SESS $FORCE_ARGS
 
 python3 -m isaac_toolkit.frontend.elf.riscv $RUN/generic_mlonmcu --session $SESS $FORCE_ARGS
 python3 -m isaac_toolkit.frontend.linker_map $RUN/mlif/generic/linker.map --session $SESS $FORCE_ARGS
-python3 -m isaac_toolkit.frontend.instr_trace.etiss $RUN/etiss_instrs.log --session $SESS --operands $FORCE_ARGS
+python3 -m isaac_toolkit.frontend.instr_trace.etiss $RUN/${TARGET}_instrs.log --session $SESS --operands $FORCE_ARGS
 python3 -m isaac_toolkit.frontend.disass.objdump $RUN/generic_mlonmcu.dump --session $SESS $FORCE_ARGS
 python3 -m isaac_toolkit.frontend.compile_commands.json $RUN/mlif/compile_commands.json --session $SESS $FORCE_ARGS
+# NEW: python3 -m isaac_toolkit.frontend.mlonmcu.exported_run $RUN --session $SESS $FORCE_ARGS
+# NEW: python3 -m isaac_toolkit.frontend.mlonmcu.exported_session $? --session $SESS $FORCE_ARGS
