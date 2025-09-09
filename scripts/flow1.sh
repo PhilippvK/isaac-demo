5d34e5e955af4855210773374b2b734c8cb3695f
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-09-09 07:20:33 +0200

    scripts/flow1.sh: expose backend, layout and rvv options

--- scripts/flow1.sh
+++ scripts/flow1.sh
@@ -18,8 +18,30 @@ UNROLL=${UNROLL:-auto}
 OPTIMIZE=${OPTIMIZE:-3}
 GLOBAL_ISEL=${GLOBAL_ISEL:-0}
 
-# python3 -m mlonmcu.cli.main flow run $BENCH --target etiss -c run.export_optional=1 -c etiss.compressed=0 -c etiss.atomic=0 -c etiss.fpu=double -c mlif.debug_symbols=1 -v -c mlif.toolchain=llvm -f memgraph_llvm_cdfg -c memgraph_llvm_cdfg.session=$LABEL -c memgraph_llvm_cdfg.stage=$STAGE -f llvm_basic_block_sections -f log_instrs -c log_instrs.to_file=1 -c mlif.num_threads=1 --label $LABEL-trace
+EXTRA_ARGS=""
+
+if [[ "$BACKEND" != "" ]]
+then
+    EXTRA_ARGS="$EXTRA_ARGS --backend $BACKEND"
+    if [[ "$LAYOUT" != "default" ]]
+    then
+        EXTRA_ARGS="$EXTRA_ARGS -c $BACKEND.desired_layout=$LAYOUT"
+    fi
+fi
+if [[ "$FPU" != "default" ]]
+then
+    EXTRA_ARGS="$EXTRA_ARGS -c $TARGET.fpu=$FPU"
+fi
+if [[ "$ENABLE_VEXT" == "1" ]]
+then
+    EXTRA_ARGS="$EXTRA_ARGS -f vext -c vext.vlen=$VLEN -c vext.elen=$ELEN -c vext.embedded=$EMBEDDED_VEXT"
+    if [[ "$AUTO_VECTORIZE" == "1" ]]
+    then
+        EXTRA_ARGS="$EXTRA_ARGS -f auto_vectorize"
+    fi
+fi
+
 # -f memgraph_llvm_cdfg -c memgraph_llvm_cdfg.session=$LABEL -c memgraph_llvm_cdfg.stage=$STAGE
-python3 -m mlonmcu.cli.main flow run $BENCH --target $TARGET -c run.export_optional=1 -c $TARGET.arch=$ARCH -c $TARGET.abi=$ABI -c mlif.debug_symbols=1 -v -c mlif.toolchain=llvm -f llvm_basic_block_sections -f log_instrs -c log_instrs.to_file=1 -c mlif.num_threads=$(nproc) -c mlif.unroll_loops=$UNROLL -c mlif.optimize=$OPTIMIZE -c mlif.global_isel=$GLOBAL_ISEL --label $LABEL-trace
+python3 -m mlonmcu.cli.main flow run $BENCH --target $TARGET -c run.export_optional=1 -c $TARGET.arch=$ARCH -c $TARGET.abi=$ABI -c mlif.debug_symbols=1 -v -c mlif.toolchain=llvm -f llvm_basic_block_sections -f log_instrs -c log_instrs.to_file=1 -c mlif.num_threads=$(nproc) -c mlif.unroll_loops=$UNROLL -c mlif.optimize=$OPTIMIZE -c mlif.global_isel=$GLOBAL_ISEL --label $LABEL-trace $EXTRA_ARGS
 
 python3 -m mlonmcu.cli.main export --run -f -- $RUN
