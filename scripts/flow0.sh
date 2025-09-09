d77aa53f6d613724fc5abd868637da70ec6f655e
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-09-09 07:11:24 +0200

    scripts/flow0.sh: expose backend and layout as well as vext options

--- scripts/flow0.sh
+++ scripts/flow0.sh
@@ -24,6 +24,38 @@ ARCH=${ARCH:-rv32imfd}
 ABI=${ABI:-ilp32d}
 GLOBAL_ISEL=${GLOBAL_ISEL:-0}
 
+BACKEND=${BACKEND:-""}
+LAYOUT=${LAYOUT:-default}
+ENABLE_VEXT=${ENABLE_VEXT:-0}
+VLEN=${VLEN:-1024}
+ELEN=${ELEN:-32}
+EMBEDDED_VEXT=${EMBEDDED_VEXT:-0}
+FPU=${FPU:-default}
+AUTO_VECTORIZE=${AUTO_VECTORIZE:-0}
+
+EXTRA_ARGS=""
+
+if [[ "$BACKEND" != "" ]]
+then
+    EXTRA_ARGS="$EXTRA_ARGS --backend $BACKEND"
+    if [[ "$LAYOUT" != "default" ]]
+    then
+        EXTRA_ARGS="$EXTRA_ARGS -c $BACKEND.desired_layout=$LAYOUT"
+    fi
+fi
+if [[ "$FPU" != "default" ]]
+then
+    EXTRA_ARGS="$EXTRA_ARGS -c $TARGET.fpu=$FPU"
+fi
+if [[ "$ENABLE_VEXT" == "1" ]]
+then
+    EXTRA_ARGS="$EXTRA_ARGS -f vext -c vext.vlen=$VLEN -c vext.elen=$ELEN -c vext.embedded=$EMBEDDED_VEXT"
+    if [[ "$AUTO_VECTORIZE" == "1" ]]
+    then
+        EXTRA_ARGS="$EXTRA_ARGS -f auto_vectorize"
+    fi
+fi
+
 VERBOSE=${VERBOSE:-0}
 VERBOSE_ARGS=""
 
@@ -32,4 +64,4 @@ then
     VERBOSE_ARGS="-v"
 fi
 
-python3 -m mlonmcu.cli.main flow run $BENCH --target $TARGET -c run.export_optional=1 -c $TARGET.arch=$ARCH -c $TARGET.abi=$ABI -c mlif.debug_symbols=1 $VERBOSE_ARGS -c mlif.toolchain=llvm --label $LABEL-baseline -c mlif.unroll_loops=$UNROLL -c mlif.optimize=$OPTIMIZE -c mlif.global_isel=$GLOBAL_ISEL
+python3 -m mlonmcu.cli.main flow run $BENCH --target $TARGET -c run.export_optional=1 -c $TARGET.arch=$ARCH -c $TARGET.abi=$ABI -c mlif.debug_symbols=1 $VERBOSE_ARGS -c mlif.toolchain=llvm --label $LABEL-baseline -c mlif.unroll_loops=$UNROLL -c mlif.optimize=$OPTIMIZE -c mlif.global_isel=$GLOBAL_ISEL $EXTRA_ARGS
