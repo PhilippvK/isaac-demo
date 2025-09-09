c558cb0e6b293108bfd023d178039a587ec4ede5
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-09-09 09:05:58 +0200

    docker: update hls image

--- docker/build_docker_image_hls.sh
+++ docker/build_docker_image_hls.sh
@@ -4,9 +4,11 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TOP_DIR=$(dirname $SCRIPT_DIR)
-IMAGE=hls-quickstart
+IMAGE=philippvk/hls-quickstart
 TAG=latest
+
+HLS_DIR=${HLS_DIR:-/path/to/isax-tools-integration}
 
 pwd
 
-docker build -t $IMAGE:$TAG -f $TOP_DIR/docker/Dockerfile_hls $TOP_DIR
+docker build -t $IMAGE:$TAG -f $TOP_DIR/docker/Dockerfile_hls $HLS_DIR
