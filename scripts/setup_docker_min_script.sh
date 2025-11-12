f45133a82eb1a7e4f8fd6d2d7c525062c441cc6f
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-11-12 15:38:23 +0100

    fix

--- scripts/setup_docker_min_script.sh
+++ scripts/setup_docker_min_script.sh
@@ -11,5 +11,6 @@ $SCRIPT_DIR/setup_python.sh
 $SCRIPT_DIR/setup_ccache.sh
 # $SCRIPT_DIR/setup_mgclient.sh
 # $SCRIPT_DIR/setup_llvm.sh
+$SCRIPT_DIR/download_llvm.sh
 $SCRIPT_DIR/setup_etiss.sh
 $SCRIPT_DIR/setup_mlonmcu.sh
