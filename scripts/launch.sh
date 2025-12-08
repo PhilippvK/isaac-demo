bf2a3adb501203854afabe392a558b2db5356f94
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-12-08 09:03:12 +0100

    update scripts

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -6,10 +6,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-${(%):-%N}}" )" &> /dev/nu
 TOP_DIR=$(dirname $SCRIPT_DIR)
 VENV_DIR=${VENV_DIR:-$TOP_DIR/venv}
 
-echo VENV=$VENV_DIR
-
 source $VENV_DIR/bin/activate
-
-python3 -m pip list
 
 $@
