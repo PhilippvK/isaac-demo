0ac73d3468c8dbaec10c57f78beee0f08d078d08
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-12-15 16:50:44 +0100

    add scripts/setup_memgraph_local.sh

---
+++ scripts/setup_memgraph_local.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+OS=${MEMGRAPH_OS:-"ubuntu-22.04"}
+VERSION=${MEMGRAPH_VERSION:-"3.1.1"}
+TAG="v${VERSION}"
+RELEASE=${MEMGRAPH_RELEASE:-1}
+ARCHIVE=memgraph_${VERSION}-${RELEASE}_amd64.deb
+
+cd /tmp
+
+wget https://download.memgraph.com/memgraph/$TAG/$OS/$ARCHIVE
+
+if [[ "$(whoami)" == "root" ]]
+then
+    dpkg -i $ARCHIVE
+else
+    sudo dpkg -i $ARCHIVE
+fi
+
+rm -f $ARCHIVE
+
+sudo systemctl status memgraph
