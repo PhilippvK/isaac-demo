11433098805b812361adb3f79aaf713320da3920
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-02-19 10:31:02 +0100

    add demo docker image and scripts

---
+++ scripts/demo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-${(%):-%N}}" )" &> /dev/null && pwd )
+TOP_DIR=$(dirname $SCRIPT_DIR)
+VENV_DIR=${VENV_DIR:-$TOP_DIR/venv}
+
+source $VENV_DIR/bin/activate
+
+$SCRIPT_DIR/full_flow.sh "$@"
