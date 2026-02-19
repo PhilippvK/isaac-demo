12b86b6df5bfe2e2382ecea1554947d0dba01ce2
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-02-19 12:05:54 +0100

    scripts/demo.sh: fix

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -4,16 +4,17 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-${(%):-%N}}" )" &> /dev/null && pwd )
 TOP_DIR=$(dirname $SCRIPT_DIR)
+SCRIPTS_DIR=$TOP_DIR/scripts
 VENV_DIR=${VENV_DIR:-$TOP_DIR/venv}
 
 CONFIG=${CONFIG:-""}
 
 # source $VENV_DIR/bin/activate
-source $SCRIPT_DIR/env.sh
+source $SCRIPTS_DIR/env.sh
 
 if [[ -f "$CONFIG" ]]
 then
     source $CONFIG
 fi
 
-$SCRIPT_DIR/full_flow.sh "$@"
+$SCRIPTS_DIR/full_flow.sh "$@"
