52b6ea38e9dd0709bcdb176d88d873b73f07c801
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-02-20 10:45:07 +0100

    add docker helpers

---
+++ docker/build_all_docker_images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-${(%):-%N}}" )" &> /dev/null && pwd )
+TOP_DIR=$(dirname $SCRIPT_DIR)
+DOCKER_DIR=$TOP_DIR/docker
+
+DOCKER_NAMESPACE=philippvk
+
+DOCKER_IMAGE_PREFIX=isaac-quickstart-
+
+DOCKER_IMAGE_NAMES=(base extra etiss etiss_perf seal5 mlonmcu mlonmcu_min min full demo)
+
+TAG=${1:-"latest"}
+
+for name in "${DOCKER_IMAGE_NAMES[@]}"; do
+    DOCKER_IMAGE=$DOCKER_NAMESPACE/${DOCKER_IMAGE_PREFIX}${name}
+    echo $DOCKER_DIR/build_docker_image_${name}.sh
+done
---
+++ docker/publish_all_docker_images.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-${(%):-%N}}" )" &> /dev/null && pwd )
+TOP_DIR=$(dirname $SCRIPT_DIR)
+DOCKER_DIR=$TOP_DIR/docker
+
+DOCKER_NAMESPACE=philippvk
+
+DOCKER_IMAGE_PREFIX=isaac-quickstart-
+
+DOCKER_IMAGE_NAMES=(base extra etiss etiss_perf seal5 mlonmcu mlonmcu_min min full demo)
+
+TAG=${1:-"latest"}
+
+for name in "${DOCKER_IMAGE_NAMES[@]}"; do
+    DOCKER_IMAGE=$DOCKER_NAMESPACE/${DOCKER_IMAGE_PREFIX}${name}
+    echo "DOCKER_IMAGE=$DOCKER_IMAGE"
+    if [[ "$TAG" != "" ]]
+    then
+        echo docker tag $DOCKER_IMAGE:latest $DOCKER_IMAGE:$TAG
+    fi
+    echo docker push $DOCKER_IMAGE:$TAG
+done
