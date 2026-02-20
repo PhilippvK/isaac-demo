822c777bf90ac132328d79fe8743114a064e2890
jokap11 <johannes.kappes@tuwien.ac.at>
2026-02-20 16:02:05 +0100

    [Docker] Swithc base docker image with a custom user

--- docker/build_docker_image_base.sh
+++ docker/build_docker_image_base.sh
@@ -7,6 +7,11 @@ TOP_DIR=$(dirname $SCRIPT_DIR)
 IMAGE=philippvk/isaac-quickstart-base
 TAG=latest
 
+USERNAME=isaac-dev
+USER_UID=$(id -u)
+USER_GID=$(id -g)
+
+
 pwd
 
-docker build -t $IMAGE:$TAG -f $TOP_DIR/docker/Dockerfile_base $TOP_DIR
+docker build --build-arg USERNAME=$USERNAME --build-arg USER_UID=$USER_UID --build-arg USER_GID=$USER_GID -t $IMAGE:$TAG -f $TOP_DIR/docker/Dockerfile_base $TOP_DIR
